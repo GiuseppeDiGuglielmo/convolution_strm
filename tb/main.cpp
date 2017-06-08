75b1a931e39f3958fb3d9f1ee88bb905d7334cc0
Giuseppe Di Guglielmo <giuseppe.diguglielmo@columbia.edu>
2017-06-08 12:13:22 -0400

    Improve testbench.
    Move most of the testbench functionalities in helper functions.
    
    Signed-off-by: Giuseppe Di Guglielmo <giuseppe.diguglielmo@columbia.edu>

--- tb/main.cpp
+++ tb/main.cpp
@@ -13,6 +13,39 @@
 #endif
 #define K 3
 
+void set_input_data(hls::stream<data_t> &stream, data_t *matrix, unsigned n_data)
+{
+	for (unsigned idx = 0; idx < n_data; idx++)
+	{
+		matrix[idx] = idx;
+		stream << idx;
+	}
+}
+
+void set_impulse_coefficient(data_t* coeff)
+{
+	for (unsigned k = 0; k < K; k++)
+		coeff[k] = 0;
+	coeff[K/2] = 1;
+}
+
+void run_validation(data_t *ref, data_t *imp, unsigned n_data)
+{
+	unsigned errors = 0;
+	for (unsigned idx = 0; idx < n_data; idx++)
+		if (ref[idx] != imp[idx])
+			errors++;
+	if (errors > 0)
+		std::cout
+				<< "ERROR: Reference and implementation matrices do no match! ("
+				<< errors << " errors out of " << n_data
+				<< " elements)" << std::endl;
+	else
+		std::cout
+				<< "INFO: Reference and implementation perfectly match! (no errors out of "
+				<< n_data << " elements)" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	data_t *src = new data_t[IMG_ROWS * IMG_COLS];
@@ -21,46 +54,19 @@ int main(int argc, char** argv)
 	data_t *hcoeff = new data_t[K];
 	data_t *vcoeff = new data_t[K];
 
-#if 0
-	unsigned errors = 0;
 	hls::stream<data_t> src_stream("src_stream");
 	hls::stream<data_t> dst_stream("dst_stream");
-#endif
 
-	// Initialize input stream (and input matrix)
-	for (unsigned row = 0; row < IMG_ROWS; row++)
-	{
-		for (unsigned col = 0; col < IMG_COLS; col++)
-		{
-			unsigned idx = row * IMG_COLS + col;
-			src[idx] = idx;
-#if 0
-			src_stream << idx;
-#endif
-		}
-	}
-
-	// Set the coefficients
-#if 1
-	// Impulse
-	for (unsigned k = 0; k < K; k++)
-		hcoeff[k] = 0;
-	for (unsigned k = 0; k < K; k++)
-		vcoeff[k] = 0;
-	hcoeff[K/2] = 1;
-	vcoeff[K/2] = 1;
-#else
-	for (unsigned k = 0; k < K; k++)
-		hcoeff[k] = 1;
-	for (unsigned k = 0; k < K; k++)
-		vcoeff[k] = 1;
-#endif
+	set_input_data(src_stream, src, IMG_ROWS*IMG_COLS);
+	set_impulse_coefficient(hcoeff);
+	set_impulse_coefficient(vcoeff);
 
 #ifdef VERBOSE
 	print_matrix(std::cout, IMG_ROWS, IMG_COLS, "Input matrix", src);
 	print_matrix(std::cout, K, 1, "Horizontal coefficients", hcoeff);
 	print_matrix(std::cout, 1, K, "Vertical coefficients", vcoeff);
 #endif
+
 #if 0
 	convolution_strm(IMG_COLS, IMG_ROWS, src_stream, dst_stream, hcoeff, vcoeff);
 
@@ -72,36 +78,19 @@ int main(int argc, char** argv)
 			dst_stream.read(dst[row * IMG_COLS + col]);
 		}
 	}
-#endif
 #ifdef VERBOSE
 	print_matrix(std::cout, IMG_ROWS, IMG_COLS, "Implementation output matrix", dst);
 #endif
+	#endif
 
-        // Validation
-        convolution_orig(IMG_ROWS, IMG_COLS, K, src, orig_dst, hcoeff, vcoeff);
+    // Validation
+    convolution_orig(IMG_ROWS, IMG_COLS, K, src, orig_dst, hcoeff, vcoeff);
 
 #ifdef VERBOSE
 	print_matrix(std::cout, IMG_ROWS, IMG_COLS, "Reference output matrix", orig_dst, K, true);
 #endif
 
-#if 0
-	errors = 0;
-	for (unsigned row = 0; row < IMG_ROWS; row++) {
-		for (unsigned col = 0; col < IMG_ROWS; col++) {
-			if (dst[row * IMG_COLS + col] != orig_dst[row * IMG_COLS + col])
-				errors++;
-		}
-	}
-	if (errors > 0)
-		std::cout
-				<< "ERROR: Reference and implementation matrices do no match! ("
-				<< errors << " errors out of " << (IMG_ROWS * IMG_COLS)
-				<< " elements)" << std::endl;
-	else
-		std::cout
-				<< "INFO: Reference and implementation perfectly match! (no errors out of "
-				<< (IMG_ROWS * IMG_COLS) << " elements)" << std::endl;
-#endif
+	//run_validation(orig_dst, dst, IMG_ROWS*IMG_COLS);
 
 	delete[] src;
 	delete[] dst;
